584f12e8894cb06723d8bb6da9966cd2900393ea
khirulalamtitu <khirulalam01@gmail.com>
2016-05-24 04:23:59 +0600

    Update NewtonReaphson.m

--- NewtonReaphson.m
+++ NewtonReaphson.m
@@ -1,14 +1,15 @@
-%fx=@(x) 6*x^3-5*x^2+7*x-2;
+%Newton-Raphson method to estimate the root of a equation
+%@Md Khirul Alam Titu
 %24/5/2016
-fx=[6 -5 7 -2];
-xold=1;
- disp(' iteration   xold        error');
-for k=1:10;
+syms x;
+f=6*x^3-5*x^2+7*x-2; %put your desire equation.suppose exp(-x)-x ;
+fd=diff(f);%First derivative
+xold=1;  %initial guess
+for k=1:5;
     xnew=xold;
-    fd=polyder(fx);
-    xold=xold-polyval(fx,xold)/polyval(fd,xold);
-     
-    error=abs(xnew-xold)/xnew*100;
-  
-   disp([k     xold        error]);   
+    xold=xold-subs(f,xold)/subs(fd,xold);
+    error=abs((xnew-xold)/xnew)*100;%approximate percentage relative error
+    [xold  error] %dispaly
+    
+    
 end
